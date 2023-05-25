b6e1546fad33aed71f62607391c4d20b66034680
ljy <ljy.sj@qq.com>
2023-05-25 11:25:20 +0800

    '

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 all:
 	g++ main.cpp -o main -pthread -g -std=c++11
+
+test:
+	g++ test.cpp -o test -pthread -g -std=c++11
--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     shared_ptr<ThreadPool> tp = make_shared<ThreadPool>(0, 100);
     tp->SetStart();
     auto res = make_shared<vector<future<int>>>();
-    for(int i = 0; i < 1000000; i++) {
+    for(int i = 0; i < 10000; i++) {
         res->push_back(tp->AddTask(addf, i, 1000));
         if(i != 0 && i % 100000 == 0) {
             sleep(5);
@@ -26,6 +26,6 @@ int main() {
     for(auto &f : *res) {
         f.wait();
     }
-    if(ans.size() != 1000000) cout << "ans.size(): " << ans.size() << endl;
+    if(ans.size() != 10000) cout << "ans.size(): " << ans.size() << endl;
     return 0;
 }
